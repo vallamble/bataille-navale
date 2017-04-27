ed58186d16ff9c554c01805558a074e15e91f8ac
yasar demir <yasarfb66@hotmail.fr>
2017-04-27 18:27:52 +0200

    code pour nihad

--- src/cad/model/Board.java
+++ src/cad/model/Board.java
@@ -2,8 +2,17 @@ package cad.model;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import sun.security.tools.keytool.Main;
 
 public class Board extends JPanel{
 	
@@ -35,6 +44,30 @@ public class Board extends JPanel{
 					this.add(plateau[i][j]);
 				}
 			}
+		//pour nihad a supp//
+		setimage(9,1);
+		}
+	
+	//////pour nihad a supp d ici//
+	public void setimage(int x,int y){
+		URL img= Main.class.getResource("/cad/a.PNG");
+		try {
+			BufferedImage bimg = ImageIO.read(img);
+	        int w=bimg.getWidth();
+	        for(int j=0;j<5;j++){//nbre de case
+                BufferedImage wim = bimg.getSubimage(j*50,0,50,50);
+                //50 car les cases font une taille de 50*50
+                Image sc=wim.getScaledInstance(50,50, Image.SCALE_AREA_AVERAGING);
+        		this.plateau[x][y].setIcon(new ImageIcon(sc));
+        		y++;
+        }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
+	
+	}
+
+
 
